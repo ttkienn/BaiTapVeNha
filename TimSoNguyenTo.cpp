5aff54b699a08cf9c6cbc731754c0673053f6e8e
Thiệu Trung Kiên <87187870+ttkienn@users.noreply.github.com>
2023-10-25 12:19:32 +0700

    Create TimSoNguyenTo.cpp

---
+++ TimSoNguyenTo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            std::cout << i << std::endl;
+        }
+    }
+    
+    return 0;
+}
